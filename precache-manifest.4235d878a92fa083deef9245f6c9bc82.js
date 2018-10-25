f8a347f5b08fa4a61aaf53b9996c9cee4354d39b
MidouDev <htmidou@gmail.com>
2018-10-25 12:44:25 +0100

    Updates

---
+++ precache-manifest.4235d878a92fa083deef9245f6c9bc82.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/exercice-clock/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "7070c00e2b5fbf3ed774",
+    "url": "/exercice-clock/static/css/main.853b2351.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/exercice-clock/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "54c964a19cdc9292523b",
+    "url": "/exercice-clock/static/js/1.54c964a1.chunk.js"
+  },
+  {
+    "revision": "ad4c1638e437bc754353",
+    "url": "/exercice-clock/static/js/runtime~main.ad4c1638.js"
+  },
+  {
+    "revision": "7070c00e2b5fbf3ed774",
+    "url": "/exercice-clock/static/js/main.7070c00e.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/exercice-clock/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/exercice-clock/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/exercice-clock/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "54c964a19cdc9292523b",
+    "url": "/exercice-clock/static/css/1.cf0fc291.chunk.css"
+  },
+  {
+    "revision": "2f85571f980b5c33aa778f132e24e161",
+    "url": "/exercice-clock/index.html"
+  }
+];
