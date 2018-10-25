bdfaa5981a4938dc4dac9e4a569531eefaae7052
MidouDev <htmidou@gmail.com>
2018-10-25 11:51:26 +0100

    Adding Proptypes to Display.js Switch.js

--- src/Display.js
+++ src/Display.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import './Display.css';
+import PropTypes from 'prop-types';
 
-const Display = ({currentTime,currentDate,isChecked}) => (
+const Display = (props) => (
     <div className="circle mx-auto mt-5">
         <div>
-            <div className="time text-center"> {currentTime} </div>
+            <div className="time text-center"> {props.currentTime} </div>
                 {
-                    isChecked 
+                    props.isChecked 
                     ?
-                    <div className="date text-center"> {currentDate} </div> 
+                    <div className="date text-center"> {props.currentDate} </div> 
                     :
                     null
                 }
         </div>
     </div>
-
 ) 
+
+Display.defaultProps = {
+    currentTime : '__:__:__',
+    currentDate : '___ ___ __ ____',
+    isChecked : true
+}
+
+Display.propTypes = {
+    currentTime : PropTypes.string,
+    currentDate : PropTypes.string,
+    isChecked : PropTypes.bool
+}
     
 export default Display;
--- src/Switch.js
+++ src/Switch.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Switch.css';
+import PropTypes from 'prop-types';
 
-const Switch = ({isChecked,handleChange}) => (
+const Switch = (props) => (
     <div className="container">
         <div className="d-flex mx-auto">
             <div className="row m-auto">
                 <label className="switch">
-                    <input type="checkbox" checked={isChecked} onChange={handleChange} />
+                    <input type="checkbox" checked={props.isChecked} onChange={props.handleChange} />
                     <span className="slider round"></span>
                 </label>
                 <i className="fa fa-calendar ml-2 fa-2x"></i>
@@ -14,5 +15,14 @@ const Switch = ({isChecked,handleChange}) => (
         </div>
     </div>
 )
+
+Switch.defaultProps = {
+    isChecked : true
+}
+
+Switch.propTypes = {
+    isChecked : PropTypes.bool,
+    handleChange : PropTypes.func
+}
 
 export default Switch;
